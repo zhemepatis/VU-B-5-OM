693a2045d773ea2cbefadf16216d1abccd3c0925
zhemepatis <rink.gab@gmail.com>
2024-09-22 16:21:36 +0300

    insignificant

--- 01-lab/main.m
+++ 01-lab/main.m
@@ -32,7 +32,7 @@ figure(3);
 plot_function(f, interest_left_end, interest_right_end);
 plot_selected_points(f, newton_method_x_values);
 
-% % Writing selected points to excel
+% Writing selected points to excel
 write_bisection_method(bisection_method_x_values);
 write_golden_ratio_method(bisection_method_x_values);
 write_newton_method(newton_method_x_values);
