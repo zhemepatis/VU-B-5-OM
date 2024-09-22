270a401bef2bd3390463f6159871e86d7d8f0d5e
zhemepatis <rink.gab@gmail.com>
2024-09-22 11:08:53 +0300

    displaying results

--- 01-lab/main.m
+++ 01-lab/main.m
@@ -3,10 +3,22 @@ clearvars, clc, close all
 % Declaring function
 interest_left_end = 0;
 interest_right_end = 10;
+x0 = 5;
 
 f = @(x) ((x.^2 - 1).^2) / 5 - 1;
-[xm, ym] = newton_method(f, 5);
+[bisection_method_xm, bisection_method_ym] = bisection_method(f, interest_left_end, interest_right_end);
+[golden_ratio_method_xm, golden_ratio_method_ym] = golden_ratio_method(f, interest_left_end, interest_right_end);
+[newton_method_xm, newton_method_ym] = newton_method(f, x0);
 
 % Results
-disp("x_m: " + xm);
-disp("y_m: " + ym);
+disp("BISECTION METHOD");
+disp("x_m: " + bisection_method_xm); 
+disp("y_m: " + bisection_method_ym);
+
+disp("GOLDEN RATIO METHOD");
+disp("x_m: " + golden_ratio_method_xm); 
+disp("y_m: " + golden_ratio_method_ym);
+
+disp("NEWTON METHOD");
+disp("x_m: " + newton_method_xm); 
+disp("y_m: " + newton_method_ym);
