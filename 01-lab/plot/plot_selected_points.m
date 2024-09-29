0c0f3d08e7397647f4cd3280b35b986b96b9e1c6
zhemepatis <rink.gab@gmail.com>
2024-09-29 16:10:17 +0300

    added legend to plotting

--- 01-lab/plot/plot_selected_points.m
+++ 01-lab/plot/plot_selected_points.m
@@ -1,7 +1,6 @@
 function plot_selected_points(f, selected_points)
-    graph = plot(selected_points, f(selected_points)); hold on;
-    graph.Color = '#404dff';
-    graph.LineStyle = "none";
-    graph.Marker = '.';
-    graph.MarkerSize = 20;
+    graph = scatter(selected_points, f(selected_points), "filled"); hold on;
+    graph.MarkerFaceColor = '#404DFF';
+
+    legend([graph], {'x_i'})
 end
